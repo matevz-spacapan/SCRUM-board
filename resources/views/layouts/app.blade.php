d54c8b19c6915053915ade666ee5a5108f8bc2c5
Matjaz Rupnik <mr8339@student.uni-lj.si>
2021-03-08 11:17:14 +0100

    Front page and CSS setup2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="#" onclick="event.preventDefault(); document.getElementById('settings-form').submit();">
+                                        {{ __('Settings') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
