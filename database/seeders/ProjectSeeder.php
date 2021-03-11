628978b813eba51a437d53ce7cd5c26471865df3
Matevž Špacapan <matevz.spacapan@gmail.com>
2021-03-11 17:40:17 +0100

    Bug fix
    Disabled inserting a story with the same title on a project.
    Slightly edited seeder for project to bug fix more easily.

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -22,21 +22,19 @@ class ProjectSeeder extends Seeder
         ]);
         Project::create([
             'project_name' => 'Second project',
-            'product_owner' => 2,
-            'project_master' => 3
+            'product_owner' => 3,
+            'project_master' => 1
         ]);
         Project::create([
             'project_name' => 'Third project',
-            'product_owner' => 3,
-            'project_master' => 1
+            'product_owner' => 2,
+            'project_master' => 3
         ]);
 
         DB::table('project_user')->insert([
             ['project_id' => 1, 'user_id' => 1],
             ['project_id' => 1, 'user_id' => 2],
-            ['project_id' => 1, 'user_id' => 3],
             ['project_id' => 2, 'user_id' => 1],
-            ['project_id' => 2, 'user_id' => 2],
             ['project_id' => 2, 'user_id' => 3],
             ['project_id' => 3, 'user_id' => 1],
             ['project_id' => 3, 'user_id' => 2],
