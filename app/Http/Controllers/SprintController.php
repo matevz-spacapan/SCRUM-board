ceda731e0e4cb26fe7e28323f0d6fe9cf6bf829f
klemen kobau <klemen.kobau@gmail.com>
2021-03-11 16:34:20 +0100

    project routing

--- app/Http/Controllers/SprintController.php
+++ app/Http/Controllers/SprintController.php
@@ -2,58 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use App\Models\Sprint;
+use App\Models\Story;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SprintController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Project $project)
     {
-        //
+        $sprints = DB::select("SELECT * from sprints WHERE project_id={$project->id}");
+        return view('sprint.index', ['sprints'=>$sprints]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Project $project)
     {
-        //
+        Project::findOrFail($project->id);
+        $this->authorize('create', [Sprint::class, $project]);
+        return view('sprint.create', ['id' => $project->id]);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, Project $project)
     {
-        //
-    }
+        Project::findOrFail($project->id);
+        $this->authorize('create', [Sprint::class, $project]);
+        $request->request->add(['project_id' => $project->id]);
+        $data = $request->validate([
+            'project_id' => ['required', 'numeric', 'min:0'],
+            'speed' => 'required|numeric|min:1',
+            'start_date' => 'required|date|after_or_equal:today',
+            'end_date' => 'required|date|after_or_equal:today'
+        ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Sprint  $sprint
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Sprint $sprint)
-    {
-        //
+        Sprint::create($data);
+
+        return redirect()->route('sprint.index');
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Sprint  $sprint
-     * @return \Illuminate\Http\Response
      */
     public function edit(Sprint $sprint)
     {
@@ -63,9 +64,6 @@ class SprintController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Sprint  $sprint
-     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Sprint $sprint)
     {
@@ -75,8 +73,6 @@ class SprintController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Sprint  $sprint
-     * @return \Illuminate\Http\Response
      */
     public function destroy(Sprint $sprint)
     {
--- app/Models/Sprint.php
+++ app/Models/Sprint.php
@@ -11,8 +11,8 @@ class Sprint extends Model
 
     protected $fillable = [
         'project_id',
-        'speed' => 'required|numeric|min:1',
-        'start_date' => 'required|date|after_or_equal:today',
-        'end_date' => 'required|date|after_or_equal:today'
+        'speed',
+        'start_date',
+        'end_date'
     ];
 }
