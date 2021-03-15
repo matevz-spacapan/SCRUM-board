2bb65956edbf5c97f0adea71b2a1cc351cf53b01
klemen kobau <klemen.kobau@gmail.com>
2021-03-15 17:22:14 +0100

    correct layout

--- resources/views/story/create.blade.php
+++ resources/views/story/create.blade.php
@@ -26,19 +26,18 @@
                                     </div>
                                     <input id="hash" type="text" class="form-control @error('hash') is-invalid @enderror col-2" name="hash" value="{{ old('hash') }}" style="margin-left:-2px">
                                     <input id="title" type="text" class="form-control @error('title') is-invalid @enderror col" name="title" value="{{ old('title') }}" style="margin-left:-2px" required autofocus>
+                                    @error('hash')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+
+                                    @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
                                 </div>
-
-                                @error('hash')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                                @error('title')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
 
