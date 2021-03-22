4d06d63b6ff7011178f64545a884f5901253346b
Matevž Špacapan <matevz.spacapan@gmail.com>
2021-03-22 22:51:41 +0100

    package-lock from main
    Remove unused imports in TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,9 +9,6 @@ use App\Models\Task;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
 use Illuminate\Support\Arr;
 
 class TaskController extends Controller
