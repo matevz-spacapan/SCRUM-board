53cd8b5d9edbc1b6c371ac8f880cc47785ae857c
Matevž Špacapan <matevz.spacapan@gmail.com>
2021-03-22 22:56:25 +0100

    Update TaskPolicy.php
    Admin can bypass the rules

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -12,6 +12,20 @@ class TaskPolicy
     use HandlesAuthorization;
 
     /**
+     * Perform pre-authorization checks.
+     *
+     * @param  \App\Models\User  $user
+     * @param  string  $ability
+     * @return void|bool
+     */
+    public function before(User $user, $ability)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
+    
+    /**
      * Determine whether the user can view any models.
      *
      * @param  \App\Models\User  $user
