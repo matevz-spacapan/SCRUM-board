2f6230a16ed30a9a52de7f779b6cec6853e6a4ac
klemen kobau <klemen.kobau@gmail.com>
2021-04-11 20:55:02 +0200

    hopefully good to go

--- resources/views/project/docs.blade.php
+++ resources/views/project/docs.blade.php
@@ -3,7 +3,15 @@
 @section('title', __(' - Documentation'))
 
 @section('content')
-    <div class="container card p-6">
-        @markdown($project->documentation)
+    <div class="container">
+        <div class="d-flex flex-row mr-2 mt-2 justify-content-between">
+            <h4 class="documentation-header">{{ __('Documentation') }}</h4>
+            <a href="{{ route('project.edit_docs_view', $project->id) }}"
+               class="btn btn-primary mb-2" {{ Popper::arrow()->position('right')->pop("Edit documentation") }}>Edit
+                documentation</a>
+        </div>
+        <div class="card p-6">
+            @markdown($project->documentation)
+        </div>
     </div>
 @endsection
--- resources/views/project/edit_docs.blade.php
+++ resources/views/project/edit_docs.blade.php
@@ -7,7 +7,11 @@
         <form method="POST" action="{{ route('project.edit_docs', $project->id) }}">
             @csrf
             <div class="d-flex flex-column justify-content-center">
-                <label for="documentation" class="mr-auto ml-auto">{{ __('Documentation') }}</label>
+                <div class="d-flex flex-row m-auto">
+                    <label for="documentation" class="m-0 text-center mr-4">{{ __('Documentation') }}</label>
+                    <a class="btn btn-link p-0"
+                       href="{{ route('project.docs', [$project->id]) }}" {{ Popper::arrow()->position('right')->pop('Discard the form and return to the documentation.') }}>{{ __('Go back') }}</a>
+                </div>
                 <textarea id="documentation" type="text" class="form-control documentation-textarea"
                           name="documentation" autofocus>
                     {{$project->documentation}}
