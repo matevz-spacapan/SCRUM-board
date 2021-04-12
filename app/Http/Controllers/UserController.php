a9823d88ec6fcdeb7487f9e5715e01b3a396191e
Matjaz Rupnik <mr8339@student.uni-lj.si>
2021-04-12 12:02:48 +0200

    Adding own user settings

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,18 @@ use DB;
 use Hash;
 use Illuminate\Support\Arr;
 use Validator;
-    
+use Auth;
+
 class UserController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+        $this->middleware('permission:users-list|users-create|users-edit|users-delete', ['only' => ['index','store', 'show']]);
+        $this->middleware('permission:users-create', ['only' => ['create','store']]);
+        $this->middleware('permission:users-edit', ['only' => ['edit','create','store']]);// ,'update'
+        $this->middleware('permission:users-delete', ['only' => ['destroy']]);
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -72,6 +81,7 @@ class UserController extends Controller
         return view('users.show',compact('user'));
     }
     
+    
     /**
      * Show the form for editing the specified resource.
      *
@@ -79,10 +89,28 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function edit($id){
+        if(!Auth::user()->isAdmin()){
+            abort(403);
+        }
+        
         $user = User::find($id);
         $roles = Role::pluck('name','name')->all();
         $userRole = $user->roles->pluck('name','name')->all();
+        
+        return view('users.edit',compact('user','roles','userRole'));
+    }
     
+    /**
+     * Show the form for editing users own data.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function editOwn(){
+        
+        $user = User::find(Auth::user()->id);
+        $roles = Role::pluck('name','name')->all();
+        $userRole = $user->roles->pluck('name','name')->all();
+        
         return view('users.edit',compact('user','roles','userRole'));
     }
     
@@ -94,6 +122,12 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id){
+        
+        // if user not admin and user tries to update another user -> abort
+        if (!Auth::user()->isAdmin() && $id != Auth::user()->id) {
+            abort(403);
+        }
+        
         $this->validate($request, [
             'username' => 'required|unique:users,username,'.$id,
             'name' => 'required',
@@ -113,10 +147,12 @@ class UserController extends Controller
         $user = User::find($id);
         $user->update($input);
         DB::table('model_has_roles')->where('model_id',$id)->delete();
-    
         $user->assignRole($request->input('roles'));
-    
-        return redirect()->route('users.index')->with('success','User updated successfully');
+        
+        if ($user->isAdmin()) {
+            return redirect()->route('users.index')->with('success','User updated successfully');
+        }
+        return redirect()->route('home');
     }
     
     /**
