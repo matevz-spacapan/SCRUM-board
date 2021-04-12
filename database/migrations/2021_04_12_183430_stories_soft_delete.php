cd891731d7d81f6b8c9db21a6da654e901b1fa89
Toncaw <licen.blaz@gmail.com>
2021-04-12 20:35:36 +0200

    added soft delete for tasks and stories

---
+++ database/migrations/2021_04_12_183430_stories_soft_delete.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class StoriesSoftDelete extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stories', function($table) {
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
