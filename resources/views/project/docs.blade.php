db01bebad9975dbfe87e5a1aab1260e78a1b8bfb
klemen kobau <klemen.kobau@gmail.com>
2021-04-12 16:07:10 +0200

    can download the documentation

--- resources/views/project/docs.blade.php
+++ resources/views/project/docs.blade.php
@@ -4,10 +4,13 @@
 
 @section('content')
     <div class="container">
-        <div class="d-flex flex-row mr-2 mt-2 justify-content-between">
-            <h4 class="documentation-header">{{ __('Documentation') }}</h4>
+        <div class="d-flex flex-row mr-2 mt-2">
+            <h4 class="documentation-header mr-auto">{{ __('Documentation') }}</h4>
             <a href="{{ route('project.edit_docs_view', $project->id) }}"
-               class="btn btn-primary mb-2" {{ Popper::arrow()->position('right')->pop("Edit documentation") }}>Edit
+               class="btn btn-primary mb-2 mr-1" {{ Popper::arrow()->position('left')->pop("Edit documentation") }}>Edit
+                documentation</a>
+            <a href="{{ route('project.download_docs', $project->id) }}"
+               class="btn btn-primary mb-2" {{ Popper::arrow()->position('right')->pop("Download documentation") }}>Download
                 documentation</a>
         </div>
         <div class="card p-6">
